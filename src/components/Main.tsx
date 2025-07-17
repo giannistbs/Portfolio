e7b3e677cbbca67a4669b2f789c8d149c4e88205
giannistbs <giannistampakis04@gmail.com>
2025-07-17 19:31:20 +0300

    Update job title in Main component to reflect current role as R&D Software Engineer

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -18,7 +18,7 @@ function Main() {
             <a href="https://www.linkedin.com/in/giannis-tampakis-b1b378256/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Ioannis Tampakis</h1>
-          <p>Software Engineer</p>
+          <p>R&D Software Engineer</p>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/giannistbs" target="_blank" rel="noreferrer"><GitHubIcon/></a>
